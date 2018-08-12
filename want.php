0118b83362e3323259408806c64ffa747b714264
井上沙耶 <>
2018-08-12 15:40:14 +0800

    ほしいものを写真一覧で表示

--- want.php
+++ want.php
@@ -110,57 +110,19 @@
         </form>
       </div>
     </div>
-
-
-
         </div>
     </header>
     <!-- ヘッダー終わり -->
 
 
 <!--wrap始まり-->
-<div id="wrap">
-    <div class="content">
-    <div id="fh5co-couple" class="fh5co-section-gray">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box">
-                    <h2>Hello!</h2>
-                    <h3>November 28th, 2016 New York, USA</h3>
-                    <p>We invited you to celebrate our wedding</p>
-                </div>
-            </div>
-            <div class="couple-wrap animate-box">
-                <div class="couple-half">
-                    <div class="groom">
-                        <img src="images/groom.jpg" alt="groom" class="img-responsive">
-                    </div>
-                    <div class="desc-groom">
-                        <h3>Joefrey Mahusay</h3>
-                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove</p>
-                    </div>
-                </div>
-                <p class="heart text-center"><i class="icon-heart2"></i></p>
-                <div class="couple-half">
-                    <div class="bride">
-                        <img src="images/bride.jpg" alt="groom" class="img-responsive">
-                    </div>
-                    <div class="desc-bride">
-                        <h3>Sheila Mahusay</h3>
-                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
 
     <div id="fh5co-gallery">
         <div class="container">
             <div class="row">
                 <div class="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box">
-                    <span>Our Memories</span>
-                    <h2>Wedding Gallery</h2>
-                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+                    <h2>your wants list</h2>
+                    <p>あなたがほしいもの</p>
                 </div>
             </div>
             <div class="row row-bottom-padded-md">
@@ -250,43 +212,7 @@
         </div>
     </div>
 
-    <div id="fh5co-started" class="fh5co-bg" style="background-image:url(images/img_bg_4.jpg);">
-        <div class="overlay"></div>
-        <div class="container">
-            <div class="row animate-box">
-                <div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
-                    <h2>Are You Attending?</h2>
-                    <p>Please Fill-up the form to notify you that you're attending. Thanks.</p>
-                </div>
-            </div>
-            <div class="row animate-box">
-                <div class="col-md-10 col-md-offset-1">
-                    <form class="form-inline">
-                        <div class="col-md-4 col-sm-4">
-                            <div class="form-group">
-                                <label for="name" class="sr-only">Name</label>
-                                <input type="name" class="form-control" id="name" placeholder="Name">
-                            </div>
-                        </div>
-                        <div class="col-md-4 col-sm-4">
-                            <div class="form-group">
-                                <label for="email" class="sr-only">Email</label>
-                                <input type="email" class="form-control" id="email" placeholder="Email">
-                            </div>
-                        </div>
-                        <div class="col-md-4 col-sm-4">
-                            <button type="submit" class="btn btn-default btn-block">I am Attending</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
 
-
-
-    </div>
-</div> 
             <!--wrap終わり-->
 
 
