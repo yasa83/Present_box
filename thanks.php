634b20334db62e379433e16feab36da5bace0b0e
DESKTOP-MKLTJ8F\maika <maikannu@yahoo.co.jp>
2018-08-12 14:28:38 +0800

    未完成

---
+++ thanks.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+        <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="css/thanks.css">
+          </head>
+
+<title>登録完了</title>
+
+<body>
+
+<div class="jumbotron text-xs-center">
+  <h1>登録ありがとうございます</h1>
+  <hr>
+  <p>
+    Having trouble? <a href="">Contact us</a>
+  </p>
+  <p class="lead">
+    <a class="btn btn-primary btn-sm" href="https://bootstrapcreative.com/" role="button">サイン</a>
+  </p>
+</div>
+</body>
