93cbf4834032269aa519b2786dd4af494a760611
井上沙耶 <>
2018-08-12 15:33:39 +0800

    写真いっぱい並ぶのコピーしてきた

--- want.php
+++ want.php
@@ -39,10 +39,10 @@
         <nav class="fh5co-nav" role="navigation">
             <div class="container">
                 <div class="row">
-                    <div class="col-xs-2">
+                    <div class="col-xs-4">
                         <div id="fh5co-logo"><a href="home.html">Present Box<strong>.</strong></a></div>
                     </div>
-                    <div class="col-xs-10 text-right menu-1">
+                    <div class="col-xs-8 text-right menu-1">
                         <ul>
                             <li><a href="＃">signout</a></li>
                             <li class="active"><a href="#">mypage</a></li>
@@ -86,7 +86,7 @@
 
           <div class="form-group">
             <label for="detail">Detail</label>
-            <input type="text" name="input_detail" class="form-control" rows="10" placeholder="どこで買えるか　なぜ欲しいのかなどを入力してください" value="">
+            <input type="text" name="input_detail" class="form-control" rows="10" placeholder="どこで買えるかなどを入力してください" value="">
           </div>
 
           <div class="form-group">
@@ -121,7 +121,167 @@
 <!--wrap始まり-->
 <div id="wrap">
     <div class="content">
+    <div id="fh5co-couple" class="fh5co-section-gray">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box">
+                    <h2>Hello!</h2>
+                    <h3>November 28th, 2016 New York, USA</h3>
+                    <p>We invited you to celebrate our wedding</p>
+                </div>
+            </div>
+            <div class="couple-wrap animate-box">
+                <div class="couple-half">
+                    <div class="groom">
+                        <img src="images/groom.jpg" alt="groom" class="img-responsive">
+                    </div>
+                    <div class="desc-groom">
+                        <h3>Joefrey Mahusay</h3>
+                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove</p>
+                    </div>
+                </div>
+                <p class="heart text-center"><i class="icon-heart2"></i></p>
+                <div class="couple-half">
+                    <div class="bride">
+                        <img src="images/bride.jpg" alt="groom" class="img-responsive">
+                    </div>
+                    <div class="desc-bride">
+                        <h3>Sheila Mahusay</h3>
+                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
+    <div id="fh5co-gallery">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box">
+                    <span>Our Memories</span>
+                    <h2>Wedding Gallery</h2>
+                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+                </div>
+            </div>
+            <div class="row row-bottom-padded-md">
+                <div class="col-md-12">
+                    <ul id="fh5co-gallery-list">
+                        
+                        <li class="one-third animate-box" data-animate-effect="fadeIn" style="background-image: url(images/gallery-1.jpg); "> 
+                        <a href="images/gallery-1.jpg">
+                            <div class="case-studies-summary">
+                                <span>14 Photos</span>
+                                <h2>Two Glas of Juice</h2>
+                            </div>
+                        </a>
+                    </li>
+                    <li class="one-third animate-box" data-animate-effect="fadeIn" style="background-image: url(images/gallery-2.jpg); ">
+                        <a href="#" class="color-2">
+                            <div class="case-studies-summary">
+                                <span>30 Photos</span>
+                                <h2>Timer starts now!</h2>
+                            </div>
+                        </a>
+                    </li>
+
+
+                    <li class="one-third animate-box" data-animate-effect="fadeIn" style="background-image: url(images/gallery-3.jpg); ">
+                        <a href="#" class="color-3">
+                            <div class="case-studies-summary">
+                                <span>90 Photos</span>
+                                <h2>Beautiful sunset</h2>
+                            </div>
+                        </a>
+                    </li>
+                    <li class="one-third animate-box" data-animate-effect="fadeIn" style="background-image: url(images/gallery-4.jpg); ">
+                        <a href="#" class="color-4">
+                            <div class="case-studies-summary">
+                                <span>12 Photos</span>
+                                <h2>Company's Conference Room</h2>
+                            </div>
+                        </a>
+                    </li>
+
+                        <li class="one-third animate-box" data-animate-effect="fadeIn" style="background-image: url(images/gallery-5.jpg); ">
+                            <a href="#" class="color-3">
+                                <div class="case-studies-summary">
+                                    <span>50 Photos</span>
+                                    <h2>Useful baskets</h2>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="one-third animate-box" data-animate-effect="fadeIn" style="background-image: url(images/gallery-6.jpg); ">
+                            <a href="#" class="color-4">
+                                <div class="case-studies-summary">
+                                    <span>45 Photos</span>
+                                    <h2>Skater man in the road</h2>
+                                </div>
+                            </a>
+                        </li>
+
+                        <li class="one-third animate-box" data-animate-effect="fadeIn" style="background-image: url(images/gallery-7.jpg); ">
+                            <a href="#" class="color-4">
+                                <div class="case-studies-summary">
+                                    <span>35 Photos</span>
+                                    <h2>Two Glas of Juice</h2>
+                                </div>
+                            </a>
+                        </li>
+
+                        <li class="one-third animate-box" data-animate-effect="fadeIn" style="background-image: url(images/gallery-8.jpg); "> 
+                            <a href="#" class="color-5">
+                                <div class="case-studies-summary">
+                                    <span>90 Photos</span>
+                                    <h2>Timer starts now!</h2>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="one-third animate-box" data-animate-effect="fadeIn" style="background-image: url(images/gallery-9.jpg); ">
+                            <a href="#" class="color-6">
+                                <div class="case-studies-summary">
+                                    <span>56 Photos</span>
+                                    <h2>Beautiful sunset</h2>
+                                </div>
+                            </a>
+                        </li>
+                    </ul>       
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="fh5co-started" class="fh5co-bg" style="background-image:url(images/img_bg_4.jpg);">
+        <div class="overlay"></div>
+        <div class="container">
+            <div class="row animate-box">
+                <div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+                    <h2>Are You Attending?</h2>
+                    <p>Please Fill-up the form to notify you that you're attending. Thanks.</p>
+                </div>
+            </div>
+            <div class="row animate-box">
+                <div class="col-md-10 col-md-offset-1">
+                    <form class="form-inline">
+                        <div class="col-md-4 col-sm-4">
+                            <div class="form-group">
+                                <label for="name" class="sr-only">Name</label>
+                                <input type="name" class="form-control" id="name" placeholder="Name">
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-sm-4">
+                            <div class="form-group">
+                                <label for="email" class="sr-only">Email</label>
+                                <input type="email" class="form-control" id="email" placeholder="Email">
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-sm-4">
+                            <button type="submit" class="btn btn-default btn-block">I am Attending</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 
