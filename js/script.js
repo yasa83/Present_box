26b8302ae02dd766c175359f461d500dcd9ef20a
hashiuchi <1510320368f@kindai.ac.jp>
2018-08-14 10:02:20 +0800

    listまとめ　indexかじり

--- js/script.js
+++ js/script.js
@@ -28,15 +28,63 @@ $(function(){
         $('#want-picture').show();
     })
 
+  /* =========================================================*/
+// jquery.modal_fix.js / ver.1.0
 
-      //モーダル
-  $('.js-modal').on('click', function() {
-    $('.modal-content').fadeIn('slow');
-    $('#modal-bg').fadeIn('slow');
-  });
+// Copyright (c) 2018 CoolWebWindow
+// This code is released under the MIT License
+// https://osdn.jp/projects/opensource/wiki/licenses%2FMIT_license
 
-  $('.js-modal-close').on('click', function() {
-    $('.modal-content').fadeOut(1000);
-    $('#modal-bg').fadeOut(1000);
-  });
-})
+// Date: 2018-01-07
+// Author: CoolWebWindow
+// Mail: info@coolwebwindow.com
+// Web: http://www.coolwebwindow.com
+
+// Used jquery.js
+// http://jquery.com/
+/* =========================================================*/
+
+    // 「.modal-open」をクリック
+    $('.modal-open').click(function(){
+        // オーバーレイ用の要素を追加
+        $('body').append('<div class="modal-overlay"></div>');
+        // オーバーレイをフェードイン
+        $('.modal-overlay').fadeIn('slow');
+
+        // モーダルコンテンツのIDを取得
+        var modal = '#' + $(this).attr('data-target');
+        // モーダルコンテンツの表示位置を設定
+        modalResize();
+        // モーダルコンテンツフェードイン
+        $(modal).fadeIn('slow');
+
+        // 「.modal-overlay」あるいは「.modal-close」をクリック
+        $('.modal-overlay, .modal-close').off().click(function(){
+            // モーダルコンテンツとオーバーレイをフェードアウト
+            $(modal).fadeOut('slow');
+            $('.modal-overlay').fadeOut('slow',function(){
+                // オーバーレイを削除
+                $('.modal-overlay').remove();
+            });
+        });
+
+        // リサイズしたら表示位置を再取得
+        $(window).on('resize', function(){
+            modalResize();
+        });
+
+        // モーダルコンテンツの表示位置を設定する関数
+        function modalResize(){
+            // ウィンドウの横幅、高さを取得
+            var w = $(window).width();
+            var h = $(window).height();
+
+            // モーダルコンテンツの表示位置を取得
+            var x = (w - $(modal).outerWidth(true)) / 2;
+            var y = (h - $(modal).outerHeight(true)) / 2;
+
+            // モーダルコンテンツの表示位置を設定
+            $(modal).css({'left': x + 'px','top': y + 'px'});
+        }
+    });
+});
