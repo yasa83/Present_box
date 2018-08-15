0a73fededc950a50595c183b7ba60bb96cc3daf7
DESKTOP-MKLTJ8F\maika <maikannu@yahoo.co.jp>
2018-08-16 00:42:11 +0800

    保存しただけ

--- check.php
+++ check.php
@@ -2,50 +2,48 @@
 <html lang="ja">
     <head>
     <meta charset="utf-8">
-        <title>Present Box</title>
+        <title>確認画面</title>
         <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
-         <link rel="stylesheet" type="text/css" href="css/check2.css">
         <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.css">
-         <link rel="icon" type="images/favicon.png" href="images/favicon.png">
-       
-             </head>
-    <body style="margin-top: 60px" class="background">
+    </head>
+    <body style="margin-top: 60px">
         <div class="container">
             <div class="row">
                 <!-- ここから -->
-                    <div class="col-xs-6 thumbnail">
-                        <h2 class="text-center content_header">Is the inputted content again?</h2>
-                        <div class="row">
-                            <div class="col-xs-4">
-                                <img src="images/check.jpg" class="img-responsive img-thumbnail">
-                            </div>
-                            <div class="col-xs-8">
-                                <div>
-                                    <span>user name</span>
-                                    <p class="lead">石原さとみ</p>
-                                </div>
-                                <div>
-                                    <span>e-mail</span>
-                                    <p class="lead">test@fooo</p>
-                                </div>
-                                <div>
-                                    <span>user ID</span>
-                                    <p class="lead">aiueo</p>
-                                <div>
-                                    <span>password</span>
-                                    <!-- ② -->
-                                    <p class="lead">●●●●●●●●</p>
-                                </div>
-                                <!-- ③ -->
-                                <form method="POST" action="thanks.php">
-                                    <!-- ⑤ -->
-                                    <input type="hidden" name="action" value="submit">
-                                    <input type="submit" class="btn btn-primary" value="user register">
-                                    <a href="signup.php" class="btn btn-default"><font color="#F14E95">&laquo;&nbsp;back</a></font>
-                                </form>
-                            </div>
-                        </div>
+            <div class="col-xs-8 col-xs-offset-2 thumbnail">
+                <h2 class="text-center content_header">アカウント情報確認</h2>
+                <div class="row">
+                    <div class="col-xs-4">
+                        <img src="users_profile_images" class="img-responsive img-thumbnail">
                     </div>
+                    <div class="col-xs-8">
+                        <div>
+                            <span>ユーザー名</span>
+                            <p class="lead">ドラえもん</p>
+                        </div>
+                        <div>
+                            <span>メールアドレス</span>
+                            <p class="lead">test@fooo</p>
+                        </div>
+                        <div>
+                            <span>パスワード</span>
+                            <!-- ② -->
+                            <p class="lead">●●●●●●●●</p>
+                        </div>
+                        <!-- ③ -->
+                        <form method="POST" action="thanks.php">
+                            <!-- ⑤ -->
+                            <input type="hidden" name="action" value="submit">
+                            <input type="submit" class="btn btn-primary" value="ユーザー登録">
+                            <a href="signup.php" class="btn btn-default">&laquo;&nbsp;戻る</a>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <!-- ここまで -->
+                <!-- <img src="../user_profile_img/"<?php echo $_SESSION['register']['img_name']; ?>"> -->
+                 
+                  </div><!-- ここにコンテンツ -->
             </div>
         </div>
         <script src="../assets/js/jquery-3.1.1.js"></script>
