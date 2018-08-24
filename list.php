e444f86f460499423a45154069bbf0de60768f24
井上沙耶 <>
2018-08-25 06:51:08 +0800

    listのmoc修正

--- list.php
+++ list.php
@@ -1,48 +1,52 @@
 <!DOCTYPE html>
 <html class="no-js"> 
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-     <link rel="icon" type="images/favicon.png" href="assets/images/favicon.png">
-    <title>Present Box</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Free HTML5 Template by FREEHTML5.CO" />
-    <meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive" />
-    <meta name="author" content="FREEHTML5.CO" />
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Present Box</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Template by FREEHTML5.CO" />
+	<meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive" />
+	<meta name="author" content="FREEHTML5.CO" />
 
-    <link href='https://fonts.googleapis.com/css?family=Work+Sans:400,300,600,400italic,700' rel='stylesheet' type='text/css'>
-    <link href="https://fonts.googleapis.com/css?family=Sacramento" rel="stylesheet">
 
-    <!-- Animate.css -->
-    <link rel="stylesheet" href="assets/css/animate.css">
-    <!-- Icomoon Icon Fonts-->
-    <link rel="stylesheet" href="assets/css/icomoon.css">
-    <!-- Bootstrap  -->
-    <link rel="stylesheet" href="assets/css/bootstrap.css">
+	<link href='https://fonts.googleapis.com/css?family=Work+Sans:400,300,600,400italic,700' rel='stylesheet' type='text/css'>
+	<link href="https://fonts.googleapis.com/css?family=Sacramento" rel="stylesheet">
 
-    <!-- Magnific Popup -->
-    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="assets/css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="assets/css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="assets/css/bootstrap.css">
 
-    <!-- Owl Carousel  -->
-    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
-    <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
-    <!-- Theme style  -->
-    <link rel="stylesheet" href="assets/css/style.css">
-    <!-- Modernizr JS -->
-    <script src="assets/js/modernizr-2.6.2.min.js"></script>
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="assets/css/magnific-popup.css">
+
+	<!-- Owl Carousel  -->
+	<link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+	<link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="assets/css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="assets/js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+		<!--[if lt IE 9]>
+		<script src="js/respond.min.js"></script>
+	<![endif]-->
+
 </head>
 <body>
-
-    <!-- ナビゲーション始まり -->
 <?php include('nav-var.php'); ?>
-    <!-- ナビゲーション終わり -->
 
 
 
 </div>
 </nav>
 
-<header id="fh5co-header" class="fh5co-cover" role="banner" style="background-image:url(images/img_bg_2.jpg);" data-stellar-background-ratio="0.5">
+<header id="fh5co-header" class="fh5co-cover" role="banner" style="background-image:url(assets/images/img_bg_2.jpg);" data-stellar-background-ratio="0.5">
 	<div class="overlay"></div>
 	<div class="container">
 		<div class="row">
@@ -92,15 +96,15 @@
 
 				<div class="row">
 					<div class="col-xs-4 ">
-						<a data-target="con1" class="modal-open"><img src="images/prezent_1.jpg" class="picture-size"></a>
+						<a data-target="con1" class="modal-open"><img src="assets/images/prezent_1.jpg" class="picture-size"></a>
 					</div>
 
 					<div class="col-xs-4">
-						<a data-target="con2" class="modal-open"><img src="images/prezent_2.jpg" class="picture-size"></a>
+						<a data-target="con2" class="modal-open"><img src="assets/images/prezent_2.jpg" class="picture-size"></a>
 					</div>
 
 					<div class="col-xs-4">
-						<a data-target="con3" class="modal-open"><img src="images/prezent_3.jpg" class="picture-size"></a>
+						<a data-target="con3" class="modal-open"><img src="assets/images/prezent_3.jpg" class="picture-size"></a>
 					</div>
 
 
@@ -108,7 +112,7 @@
 					<div id="con1" class="modal-content" style="width: 800px; height: 500px;">
 						<div class="row">
 							<div class="col-md-6">
-								<br><br><img src="images/prezent_1.jpg" class="picture-size">
+								<br><br><img src="assets/images/prezent_1.jpg" class="picture-size">
 							</div>
 							<div class="col-md-6" style="font-size: 25px; line-height: 4em;">
 								<ul class="text-left" >
@@ -126,7 +130,7 @@
 					<div id="con2" class="modal-content" style="width: 800px; height: 500px;">
 						<div class="row">
 							<div class="col-md-6">
-								<br><br><img src="images/prezent_2.jpg" class="picture-size">
+								<br><br><img src="assets/images/prezent_2.jpg" class="picture-size">
 							</div>
 							<div class="col-md-6"><p>リンク1の内容です。</p>
 								<p><a class="modal-close">閉じる</a></p>
@@ -137,7 +141,7 @@
 					<div id="con3" class="modal-content" style="width: 800px; height: 500px;">
 						<div class="row">
 							<div class="col-md-6">
-								<br><br><img src="images/prezent_3.jpg" class="picture-size">
+								<br><br><img src="assets/images/prezent_3.jpg" class="picture-size">
 							</div>
 							<div class="col-md-6"><p>リンク1の内容です。</p>
 								<p><a class="modal-close">閉じる</a></p>
@@ -160,16 +164,16 @@
 
 				<div class="row">
 					<div class="col-xs-4 ">
-						<a data-target="con4" class="modal-open"><img src="images/gallery-1.jpg" class="picture-size"></a>
+						<a data-target="con4" class="modal-open"><img src="assets/images/gallery-1.jpg" class="picture-size"></a>
 
 					</div>
 
 					<div class="col-xs-4">
-						<a data-target="con5" class="modal-open"><img src="images/friend1.png" class="picture-size"></a>
+						<a data-target="con5" class="modal-open"><img src="assets/images/friend1.png" class="picture-size"></a>
 					</div>
 
 					<div class="col-xs-4">
-						<a data-target="con6" class="modal-open"><img src="images/friend1.png" class="picture-size"></a>
+						<a data-target="con6" class="modal-open"><img src="assets/images/friend1.png" class="picture-size"></a>
 					</div>
 
 
@@ -177,7 +181,7 @@
 					<div id="con4" class="modal-content" style="width: 800px; height: 500px;">
 						<div class="row">
 							<div class="col-md-6">
-								<br><br><img src="images/gallery-1.jpg" class="picture-size">
+								<br><br><img src="assets/images/gallery-1.jpg" class="picture-size">
 							</div>
 							<div class="col-md-6"><p>リンク1の内容です。</p>
 								<p><a class="modal-close">閉じる</a></p>
@@ -188,7 +192,7 @@
 					<div id="con5" class="modal-content" style="width: 800px; height: 500px;">
 						<div class="row">
 							<div class="col-md-6">
-								<br><br><img src="images/friend1.png" class="picture-size">
+								<br><br><img src="assets/images/friend1.png" class="picture-size">
 							</div>
 							<div class="col-md-6"><p>リンク1の内容です。</p>
 								<p><a class="modal-close">閉じる</a></p>
@@ -199,7 +203,7 @@
 					<div id="con6" class="modal-content" style="width: 800px; height: 500px;">
 						<div class="row">
 							<div class="col-md-8">
-								<br><br><img src="images/friend1.png" class="picture-size">
+								<br><br><img src="assets/images/friend1.png" class="picture-size">
 							</div>
 							<div class="col-md-4"><p>リンク1の内容です。</p>
 								<p><a class="modal-close">閉じる</a></p>
@@ -219,16 +223,16 @@
 
 				<div class="row">
 					<div class="col-xs-4 ">
-						<a data-target="con7" class="modal-open"><img src="images/gallery-1.jpg" class="picture-size"></a>
+						<a data-target="con7" class="modal-open"><img src="assets/images/gallery-1.jpg" class="picture-size"></a>
 
 					</div>
 
 					<div class="col-xs-4">
-						<a data-target="con8" class="modal-open"><img src="images/friend1.png" class="picture-size"></a>
+						<a data-target="con8" class="modal-open"><img src="assets/images/friend1.png" class="picture-size"></a>
 					</div>
 
 					<div class="col-xs-4">
-						<a data-target="con9" class="modal-open"><img src="images/friend1.png" class="picture-size"></a>
+						<a data-target="con9" class="modal-open"><img src="assets/images/friend1.png" class="picture-size"></a>
 					</div>
 
 
@@ -236,7 +240,7 @@
 					<div id="con7" class="modal-content" style="width: 800px; height: 500px;">
 						<div class="row">
 							<div class="col-md-6">
-								<br><br><img src="images/gallery-1.jpg" class="picture-size">
+								<br><br><img src="assets/images/gallery-1.jpg" class="picture-size">
 							</div>
 							<div class="col-md-6">
 								<ul>
@@ -253,7 +257,7 @@
 					<div id="con8" class="modal-content" style="width: 800px; height: 500px;">
 						<div class="row">
 							<div class="col-md-6">
-								<br><br><img src="images/friend1.png" class="picture-size">
+								<br><br><img src="assets/images/friend1.png" class="picture-size">
 							</div>
 							<div class="col-md-6"><p>リンク1の内容です。</p>
 								<p><a class="modal-close">閉じる</a></p>
@@ -264,7 +268,7 @@
 					<div id="con9" class="modal-content" style="width: 800px; height: 500px;">
 						<div class="row">
 							<div class="col-md-6">
-								<br><br><img src="images/friend1.png" class="picture-size">
+								<br><br><img src="assets/images/friend1.png" class="picture-size">
 							</div>
 							<div class="col-md-6"><p>リンク1の内容です。</p>
 								<p><a class="modal-close">閉じる</a></p>
@@ -296,37 +300,55 @@
         </div>
     </footer>
     <!-- フッター終わり -->
+	</div>
 
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
 
-<!-- 画面遷移用の矢印 -->
-</div>
-<div class="gototop js-top">
-    <a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
-</div>
-</div>
+	<!-- jQuery -->
+	<script src="assets/js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="assets/js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="assets/js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="assets/js/jquery.waypoints.min.js"></script>
+	<!-- Carousel -->
+	<script src="assets/js/owl.carousel.min.js"></script>
+	<!-- countTo -->
+	<script src="assets/js/jquery.countTo.js"></script>
 
+	<!-- Stellar -->
+	<script src="assets/js/jquery.stellar.min.js"></script>
+	<!-- Magnific Popup -->
+	<script src="assets/js/jquery.magnific-popup.min.js"></script>
+	<script src="assets/js/magnific-popup-options.js"></script>
+	<!-- 	自分で作ったやつ -->
+	<script src="assets/js/script.js"></script>
 
-<!-- jQuery -->
-<script src="assets/js/jquery.min.js"></script>
-<!-- jQuery Easing -->
-<script src="assets/js/jquery.easing.1.3.js"></script>
-<!-- Bootstrap -->
-<script src="assets/js/bootstrap.min.js"></script>
-<!-- Waypoints -->
-<script src="assets/js/jquery.waypoints.min.js"></script>
-<!-- Carousel -->
-<script src="assets/js/owl.carousel.min.js"></script>
-<!-- countTo -->
-<script src="assets/js/jquery.countTo.js"></script>
+	<script src="assets/js/simplyCountdown.js"></script>
+	<!-- Main -->
+	<script src="assets/js/main.js"></script>
 
-<!-- Stellar -->
-<script src="assets/js/jquery.stellar.min.js"></script>
-<!-- Magnific Popup -->
-<script src="assets/js/jquery.magnific-popup.min.js"></script>
-<script src="assets/js/magnific-popup-options.js"></script>
+	<script>
+		var d = new Date(new Date().getTime() + 200 * 120 * 120 * 2000);
 
-<!-- Main -->
-<script src="assets/js/main.js"></script>
+    // default example
+    simplyCountdown('.simply-countdown-one', {
+    	year: d.getFullYear(),
+    	month: d.getMonth() + 1,
+    	day: d.getDate()
+    });
+
+    //jQuery example
+    $('#simply-countdown-losange').simplyCountdown({
+    	year: d.getFullYear(),
+    	month: d.getMonth() + 1,
+    	day: d.getDate(),
+    	enableUtc: false
+    });
+</script>
 
 </body>
 </html>
