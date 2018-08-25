ca232ae5a36f844daceda48bc6ad0ffc0c852df8
井上沙耶 <>
2018-08-26 06:44:08 +0800

    空のバリデーション

--- signin.php
+++ signin.php
@@ -1,3 +1,17 @@
+<?php 
+$errors = [];
+
+if(!empty($_POST)){
+    $id= $_POST['id'];
+    $password = $_POST['password'];
+
+    if($id !='' && $password !=''){
+
+    }else{
+        $errors['signin'] = 'blank';
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -8,33 +22,34 @@
 <link rel="stylesheet" type="text/css" href="assets/css/signin.css">
 </head>
 <body background="assets/images/alcohl.jpg">
-	<div class="limiter">
-		<div class="container-login100" style="">
-			<div class="wrap-login100 p-t-30 p-b-50">
-				<span class="login100-form-title p-b-41">
-					Account Login
-				</span>
-				<form class="login100-form validate-form p-b-33 p-t-5" action="#">
-					<div class="wrap-input100 validate-input" data-validate = "Enter username">
-						<input class="input100" type="text" name="username" placeholder="User ID">
-						<span class="focus-input100" data-placeholder="&#xe82a;"></span>
-					</div>
-					<div class="wrap-input100 validate-input" data-validate="Enter password">
-						<input class="input100" type="password" name="pass" placeholder="Password">
-						<span class="focus-input100" data-placeholder="&#xe80f;"></span>
-					</div>
-					<div class="container-login100-form-btn m-t-32">
-						<button class="login100-form-btn">
-							<a href="home.php">Login</a>
-						</button>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
-	<script src="assets/js/jquery.min.js"></script>
-	<script src="assets/js/bootstrap.min.js"></script>
-	<script src="assets/js/main.js"></script>
+    <div class="limiter">
+        <div class="container-login100" style="">
+            <div class="wrap-login100 p-t-30 p-b-50">
+                <span class="login100-form-title p-b-41">
+                    Account Login
+                </span>
+                <form class="login100-form validate-form p-b-33 p-t-5" method="POST" action="signin.php">
+                    <?php if(isset($errors['signin']) && $errors['signin'] == 'blank'): ?>
+                        <p style="margin:30px 5px 5px 80px; color: red;">enter id and password correctly</p>
+                    <?php endif; ?>
+                    <div class="wrap-input100 validate-input" data-validate = "Enter username">
+                        <input class="input100" type="text" name="id" placeholder="User ID">
+                        <span class="focus-input100" data-placeholder="&#xe82a;"></span>
+                    </div>
+                    <div class="wrap-input100 validate-input" data-validate="Enter password">
+                        <input class="input100" type="password" name="password" placeholder="Password">
+                        <span class="focus-input100" data-placeholder="&#xe80f;"></span>
+                    </div>
+                    <div class="container-login100-form-btn m-t-32">
+                        <input type="submit" class="btn login100-form-btn" value="Login">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/main.js"></script>
 
 </body>
 </html>
