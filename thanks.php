4a9057f19f3c33e1b7b432852ef3edfed54beea7
井上沙耶 <>
2018-08-25 23:14:17 +0800

    タグの位置修正

--- thanks.php
+++ thanks.php
@@ -1,18 +1,15 @@
 <!DOCTYPE html>
 <html lang="ja">
-        <head>
-        <meta charset="utf-8">
-        <link rel="stylesheet" type="text/css" href="assets/css/thanks.css">
-        <link rel="icon" type="images/favicon.png" href="assets/images/favicon.png">
-
-          </head>
-
-<title>Present Box</title>
-
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="assets/css/thanks.css">
+    <link rel="icon" type="images/favicon.png" href="assets/images/favicon.png">
+    <title>Present Box</title>
+</head>
 <body>
 <div>
     <p>thank you for registration</p>
 </div>
-
       <a href="signin.php" class="fancy-button"><span>signin</span></a>
 </body>
+</html>
