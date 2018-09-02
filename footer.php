8f430c8efbfee8c8da7e9ef4bf9eca29ced87014
井上沙耶 <>
2018-09-02 16:47:02 +0800

    footerをまとめた

---
+++ footer.php
@@ -0,0 +1,25 @@
+    <!-- フッター始まり -->
+    <footer id="fh5co-footer" role="contentinfo">
+        <div class="container">
+            <div class="row copyright">
+                <div class="col-md-12 text-center">
+                    <p>
+                        <small class="block">&copy; 43batch チームはしうち　Nexseed<br>
+                        FREEHTML5.CO</small> 
+                    </p>
+                    <p>
+                        <ul class="fh5co-social-icons">
+                            <li><a href="https://twitter.com/nexseed_cebu"><i class="icon-twitter"></i></a></li>
+                            <li><a href="https://www.facebook.com/NexSeed/"><i class="icon-facebook"></i></a></li>
+                        </ul>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- フッター終わり -->
+
+    <!-- 画面遷移用の矢印 -->
+    <div class="gototop js-top">
+        <a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+    </div>
--- list_make.php
+++ list_make.php
@@ -11,8 +11,6 @@ $stmt->execute($data);
 $signin_user = $stmt->fetch(PDO::FETCH_ASSOC);
 
 
-
-var_dump($_GET);die();
 //友達へのプレゼント登録
 $check = '';
 $present = '';
@@ -170,34 +168,7 @@ if(!empty($_POST)){
     </header>
 <!-- ヘッダー終わり -->
 
-    <!-- フッター始まり -->
-    <footer id="fh5co-footer" role="contentinfo">
-        <div class="container">
-            <div class="row copyright">
-                <div class="col-md-12 text-center">
-                    <p>
-                        <small class="block">&copy; 43batch チームはしうち　Nexseed<br>
-                        FREEHTML5.CO</small> 
-                    </p>
-                    <p>
-                        <ul class="fh5co-social-icons">
-                            <li><a href="https://twitter.com/nexseed_cebu"><i class="icon-twitter"></i></a></li>
-                            <li><a href="https://www.facebook.com/NexSeed/"><i class="icon-facebook"></i></a></li>
-                        </ul>
-                    </p>
-                </div>
-            </div>
-        </div>
-    </footer>
-    <!-- フッター終わり -->
-
-    <!-- 画面遷移用の矢印 -->
-    </div>
-    <div class="gototop js-top">
-        <a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
-    </div>
-    </div>
-
+    <?php include('footer.php'); ?>
     <!-- jQuery -->
     <script src="assets/js/jquery.min.js"></script>
     <!-- jQuery Easing -->
