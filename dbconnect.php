a8cfddc76c01fb24fdc0fe47d7bf3279a05d0549
Your Name <you@example.com>
2019-03-07 16:27:52 +0900

    DBコネクトを変更

--- dbconnect.php
+++ dbconnect.php
@@ -1,7 +1,7 @@
 <?php
-    $dsn = 'mysql:dbname=Toronto;host=localhost';
-    $user = 'root';
-    $password='';
+    $dsn = 'mysql://b8d29be4df14ce:31879917@us-cdbr-iron-east-03.cleardb.net/heroku_09e14818624d15f?reconnect=true';
+    $user = 'b8d29be4df14ce';
+    $password = '31879917';
     $dbh = new PDO($dsn, $user, $password);
     // SQL文にエラーがあった際、画面にエラーを出力する設定
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
