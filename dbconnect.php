0a5c91a0029ae8d9cc754e1fa34f9c657bd265e0
Your Name <you@example.com>
2019-03-07 16:34:48 +0900

    コネクト

--- dbconnect.php
+++ dbconnect.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
     $dsn = 'mysql:dbname=Toronto;host=localhost';
     $user = 'root';
     $password='';
@@ -6,4 +6,15 @@
     // SQL文にエラーがあった際、画面にエラーを出力する設定
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $dbh->query('SET NAMES utf8');
+?>
+ -->
+<?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
 ?>
