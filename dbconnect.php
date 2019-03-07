09ef59a7a808ec0c3334223b516dc9a43cf96c05
Your Name <you@example.com>
2019-03-08 00:55:09 +0900

    change

--- dbconnect.php
+++ dbconnect.php
@@ -1,21 +1,13 @@
-<!-- <?php
-    $dsn = 'mysql:dbname=Toronto;host=localhost';
-    $user = 'root';
-    $password='';
-    $dbh = new PDO($dsn, $user, $password);
-    // SQL文にエラーがあった際、画面にエラーを出力する設定
-    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $dbh->query('SET NAMES utf8');
-?>
- -->
 <?php
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
+  $db_name = getenv(DB_NAME);
+  $host_name = getenv(HOST_NAME);
+  $user = getenv(USER_NAME);
+  $password = getenv(PASSWORD);
 
-$conn = new mysqli($server, $username, $password, $db);
-$conn->query();
-?>
+  $dsn = 'mysql:dbname='.$db_name.';host=' . $host_name;
+  $dbh = new PDO($dsn, $user, $password);
+  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $dbh->query('SET NAMES utf8');
+
+  // mysql://(--username--):(--password--)@(--hostname--)/(--dbname--)?
